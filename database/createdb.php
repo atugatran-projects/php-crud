0ecce3776541f0e4b2839f1cb8a776257b0232a3
Atugatran <atugatran@gmail.com>
2022-12-20 23:05:19 +0530

    improve mql query

--- database/createdb.php
+++ database/createdb.php
@@ -9,10 +9,10 @@ $conn = mysqli_connect($servername, $username, $password);
 
 // Creating a database named irailway
 $sql = "CREATE DATABASE notes";
-if ($conn->query($sql) === TRUE) {
-}
+mysqli_query($conn, $sql);
 
 // Creating a Table ticket
 $sql2 = "CREATE TABLE `notes`.`notes` (`sno` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(255) NOT NULL , `description` VARCHAR(255) NOT NULL , `tstamp` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) , PRIMARY KEY (`sno`)) ENGINE = InnoDB;";
-if ($conn->query($sql2) === TRUE) {
-}
+mysqli_query($conn, $sql2);
+
+mysqli_close($conn);
